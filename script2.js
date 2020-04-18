05e1768459338668dacd4f1a709ec99b02df7431
Linnea <linneagear@gmail.com>
2020-04-18 09:15:27 -0700

    WHEN button is pressed THEN prompts appear

--- script2.js
+++ script2.js
@@ -3,62 +3,55 @@
 var generateBtn = document.querySelector("#generate");
 
 // // Add event listener to generate password on 'click'
-generateBtn.addEventListener("click", writePassword); 
+generateBtn.addEventListener("click", writePassword);
 
+// when button is pressed, generates password by this function
 function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
-    passwordText.value = password;
+  passwordText.value = password;
 }
 
-var confirmLength = Number(prompt("How many characters will your password be? Enter a number between 8 and 128"));
+function generatePassword() {
+  // have user enter password length
+  var confirmLength = Number(prompt("How many characters will your password be? (between 8 and 128)"));
 
-//Step 1: Ask user for input
-// when button is pressed, THEN user prompts appear
-var confirmUpper = confirm("Do you want upper case letters?")
-var confirmLower = confirm("Do you want lower case letters?")
-var confirmNumeric = confirm("Do you want to use numbers?")
-var confirmSpecial = confirm("Do you want special characters?")
+  // when button is pressed, THEN user prompts appear
+  var confirmUpper = confirm("Do you want upper case letters?")
+  var confirmLower = confirm("Do you want lower case letters?")
+  var confirmNumeric = confirm("Do you want to use numbers?")
+  var confirmSpecial = confirm("Do you want special characters?")
 
-console.log(confirmUpper);
-console.log(confirmUpper);
-console.log(confirmUpper);
-console.log(confirmUpper);
-console.log(confirmLength);
-
-//generate password
-function userCriteria() {
-
-  var arr = "";
-  if( confirmLower === true ) {
+  // conditionals of what they want
+  var arrCriteria = [];
+  if (confirmLower === true) {
     getLower = "abcdefghijklmnopqrstuvwxyz";
-  } if( confirmUpper === true ) {
+    arrCriteria.push(getLower);
+  } if (confirmUpper === true) {
     getUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  } if(confirmUpper === true) {
+    arrCriteria.push(getUpper);
+  } if (confirmNumeric === true) {
     getNumeric = "0123456789";
-  } if( confirmSpecial === true ) {
+    arrCriteria.push(getNumeric);
+  } if (confirmSpecial === true) {
     getSpecial = "!@#$%^&*(){}[]=<>/,.";
-  } 
+    arrCriteria.push(getSpecial);
+  }
 
+  for (i = 0; i < confirmLength; i++) {
+    console.log(arrCriteria[Math.floor(Math.random())]);
+  }
   //return value
-  var newPW = "";
-  for (var i = 0; i < confirmLength; i++) {
-    //picks a character within charSet at index of random number
-    newPW += getChar.charAt(Math.floor(Math.random() * getChar.length));
-  }
-  for (var i = 0; i < confirmLength; i++) {
-    //picks a character within charSet at index of random number
-    newPW += getChar.charAt(Math.floor(Math.random() * getChar.length));
-  }
-  return newPW;
+  // var newPW = " ";
+  // for (var i = 0; i < confirmLength; i++) {
+  //   //picks a character within charSet at index of random number
+  //   newPW += getChar.charAt(Math.floor(Math.random() * getChar.length));
+  // }
+  // return newPW;
 }
-console.log(userCriteria());
 
-// function generatePassword() {
-//     for (i=0; i < confirmLength; i++) {
-//         return userCriteria[Math.floor(Math.random())];
-//     }
-// }
+
+
 
 
